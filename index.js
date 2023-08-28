78522447c8cdb75c621f901533c0c950682fdd24
thetamimahmed <wwwtamim000@gmail.com>
2023-08-28 23:07:59 +0600

    added details server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgrretg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,45 +33,58 @@ async function run() {
       res.send(result);
     });
 
+    //get individual data of search
+    app.get("/searchResult/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const restaurantsResult = await restaurantsCollection.find().toArray();
+      const hotelsResult = await hotelsCollection.find().toArray();
+      const combineResults = [...restaurantsResult, ...hotelsResult];
+      const selectedCard = combineResults.find(data => data._id.toString() == id)
+      console.log(selectedCard)
+      return res.send(selectedCard);
+    });
+
     app.get("/:category/:searchText", async (req, res) => {
       const searchText = req.params.searchText;
       const category = req.params.category;
-       if(category === "hotels"){
-      const query = searchText
-        ? {
-          hotelName: { $regex: new RegExp(searchText, "i") },
-          }
-        : {};
-      const result = await hotelsCollection.find(query).toArray();
-      return res.send(result);
-    }
-     else if(category === "restaurants"){
+      if (category === "hotels") {
         const query = searchText
-        ? {
-          title: { $regex: new RegExp(searchText, "i") },
-        }
-        : {};
+          ? {
+              hotelName: { $regex: new RegExp(searchText, "i") },
+            }
+          : {};
+        const result = await hotelsCollection.find(query).toArray();
+        return res.send(result);
+      } else if (category === "restaurants") {
+        const query = searchText
+          ? {
+              title: { $regex: new RegExp(searchText, "i") },
+            }
+          : {};
         const result = await restaurantsCollection.find(query).toArray();
         return res.send(result);
-      }
-     else if(category === "search-all"){
+      } else if (category === "search-all") {
         const query = searchText
-        ? {
-          $or: [
-            { title: { $regex: new RegExp(searchText, "i") } },
-            { hotelName: { $regex: new RegExp(searchText, "i") } }
-          ]
-        }
-        : {};
-        const restaurantsResult = await restaurantsCollection.find(query).toArray();
+          ? {
+              $or: [
+                { title: { $regex: new RegExp(searchText, "i") } },
+                { hotelName: { $regex: new RegExp(searchText, "i") } },
+              ],
+            }
+          : {};
+        const restaurantsResult = await restaurantsCollection
+          .find(query)
+          .toArray();
         const hotelsResult = await hotelsCollection.find(query).toArray();
-        const combineResults = [...restaurantsResult, ...hotelsResult]
+        const combineResults = [...restaurantsResult, ...hotelsResult];
         return res.send(combineResults);
-      }
-        else{
-        return res.send([])
+      } else {
+        return res.send([]);
       }
     });
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
